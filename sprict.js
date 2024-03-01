d83a605968f2ea60b31c1338db21fb7f69200cea
Rafael <111583184+Rafaschmidt1@users.noreply.github.com>
2024-03-01 11:07:34 -0300

    kkkkkkconsole.log('ohhhh neyma')

--- sprict.js
+++ sprict.js
@@ -1,1 +1,12 @@
-console.log('ohhhh neyma')
+const botoes = document.querySelectorAll(".btn");
+
+for (let i = 0; i < botoes.length; i++) {
+    botoes[i].onclick = function () {
+
+        for (let j = 0; j < botoes.length; j++) {
+            botoes[j].classList.remove("active");
+        }
+
+        botoes[i].classList.add("active");
+    }
+}
